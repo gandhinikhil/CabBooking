cb1c71f341f5e90e611da79aaa828552c2d1f66f
Nikhil <gwl.nikhil@gmail.com>
2024-08-26 00:29:40 +0530

    Replace All Driver model object with DriverDetailsVO objects. Sorted availableDrivers list by driver current location to sources location.

--- src/main/java/com/example/cabbooking/servicetest/DriverService.java
+++ src/main/java/com/example/cabbooking/servicetest/DriverService.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 @Service
 public class DriverService {
-    private final Map<String, Driver> drivers = new HashMap<>();
+    private final Map<String, DriverDetailsVO> drivers = new HashMap<>();
     @Autowired
     @Lazy
     private RideService rideService;
@@ -26,27 +26,27 @@ public class DriverService {
     public void addDriver(DriverDetailsVO driverDetailsVO) {
         VehicleDetailsVO vehicleDetailsVO = driverDetailsVO.getVehicle();
         LocationDetailsVO locationDetailsVO = driverDetailsVO.getLocation();
-        Vehicle vehicle = new Vehicle(vehicleDetailsVO.getModel(), vehicleDetailsVO.getRegistrationNumber());
-        Location location = new Location(locationDetailsVO.getX(), locationDetailsVO.getY());
-        Driver driver = new Driver(driverDetailsVO.getName(), driverDetailsVO.getGender(), driverDetailsVO.getAge(), vehicle, location, true);
+        VehicleDetailsVO vehicle = new VehicleDetailsVO(vehicleDetailsVO.getModel(), vehicleDetailsVO.getRegistrationNumber());
+        LocationDetailsVO location = new LocationDetailsVO(locationDetailsVO.getX(), locationDetailsVO.getY());
+        DriverDetailsVO driver = new DriverDetailsVO(driverDetailsVO.getName(), driverDetailsVO.getGender(), driverDetailsVO.getAge(), vehicle, location, true);
         drivers.put(driver.getName(), driver);
-        System.out.println(driver.getName()+" "+driver.getGender()+" "+driver.getAge()+" "+driver.isAvailable()+" "+driver.getCurrentLocation()+" "+driver.getVehicle());
+        System.out.println(driver.getName()+" "+driver.getGender()+" "+driver.getAge()+" "+driver.isAvailable()+" "+driver.getLocation()+" "+driver.getVehicle());
     }
 
     public List<RideDetailsVO> findAvailableDrivers(LocationDetailsVO source,LocationDetailsVO destination, int maxDistance) {
         List<RideDetailsVO> availableDrivers = new ArrayList<>();
-        for (Driver driver : drivers.values()) {
-            if (driver.isAvailable() && driver.getCurrentLocation().distanceTo(source) <= maxDistance) {
-
+        for (DriverDetailsVO driver : drivers.values()) {
+            if (driver.isAvailable() && driver.getLocation().distanceTo(source)<= maxDistance) {
                 double fare = rideService.calculateFare(source,destination);
                 availableDrivers.add(new RideDetailsVO(driver,Math.round(fare*100.0)/100.0));
                 System.out.println(driver.getName());
             }
         }
+        rideService.sortedDriversByDistanceToSource(source,availableDrivers);
         return availableDrivers;
     }
 
-    public Driver getDriver(String driverName) {
+    public DriverDetailsVO getDriver(String driverName) {
         return drivers.get(driverName);
     }
 }
