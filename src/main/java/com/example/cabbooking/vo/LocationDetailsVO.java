b28e3a3f8891aa77329cf94a2f84166f58fd69b2
Nikhil <gwl.nikhil@gmail.com>
2024-08-26 00:31:02 +0530

    Add Setter lambok. Add distanceTO function for calculation of sources distance.

--- src/main/java/com/example/cabbooking/vo/LocationDetailsVO.java
+++ src/main/java/com/example/cabbooking/vo/LocationDetailsVO.java
@@ -2,11 +2,17 @@ package com.example.cabbooking.vo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class LocationDetailsVO {
     private int x;
     private int y;
 
+    public int distanceTo(LocationDetailsVO source) {
+        return Math.abs(this.x - x) + Math.abs(this.y - y);
+    }
 }
