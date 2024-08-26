99822ff3c5ac8c66f71dcd31d2e35fc731d06749
Nikhil <gwl.nikhil@gmail.com>
2024-08-26 23:06:06 +0530

    Update VO class with exsisting variaable.

--- src/main/java/com/example/cabbooking/vo/RideDetailsVO.java
+++ src/main/java/com/example/cabbooking/vo/RideDetailsVO.java
@@ -1,6 +1,5 @@
 package com.example.cabbooking.vo;
 
-import com.example.cabbooking.model.Driver;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/com/example/cabbooking/vo/UserDetailsInVO.java
+++ src/main/java/com/example/cabbooking/vo/UserDetailsInVO.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDetailsInVO {
-
+    private long id;
     private String userName;
-
     private String gender;
-
     private int age;
+    private LocalDateTime localDateTime = LocalDateTime.now();
 
 }
