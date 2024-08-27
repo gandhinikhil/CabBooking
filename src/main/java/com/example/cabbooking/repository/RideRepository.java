0043f82db54082768a7016cee82fb3df5c02971d
Nikhil <gwl.nikhil@gmail.com>
2024-08-28 01:59:25 +0530

    Create required optional function for getting usernae ad driver name from ride entity.

--- src/main/java/com/example/cabbooking/repository/RideRepository.java
+++ src/main/java/com/example/cabbooking/repository/RideRepository.java
@@ -1,6 +1,8 @@
 package com.example.cabbooking.repository;
 import com.example.cabbooking.entity.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface RideRepository extends JpaRepository<Ride,Long> {
+   Optional<Ride>findByUserNameAndDriverName(String userName, String driverName);
 }
