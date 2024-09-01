d5c358d1f259e993e049b6e102228d1982cef4b8
Nikhil <gwl.nikhil@gmail.com>
2024-09-01 22:15:03 +0530

    Security Authuntication

--- src/main/java/com/example/cabbooking/configfile/ConfigurationSecurity.java
+++ src/main/java/com/example/cabbooking/configfile/ConfigurationSecurity.java
@@ -1,4 +1,18 @@
 package com.example.cabbooking.configfile;
 
-public class ConfigurationSecurity {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigurationSecurity extends WebSecurityConfiguration{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll();
+            }
+
 }
